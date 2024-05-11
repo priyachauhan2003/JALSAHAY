f65cee91d11783758dd635956707c837e016aec9
Priya Chauhan <priyachauhan.kkr@gmail.com>
2024-05-11 17:09:30 +0530

    Initial commit

---
+++ app/src/main/java/com/techmania/jalsahay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.techmania.jalsahay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
