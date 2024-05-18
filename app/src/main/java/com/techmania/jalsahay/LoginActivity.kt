98d1fa81b0a05272ec9d87fe20418ef5904fd120
Vansh Punia <vanshpunia007@gmial.com>
2024-05-18 12:42:47 +0530

    home fragment started

--- app/src/main/java/com/techmania/jalsahay/LoginActivity.kt
+++ app/src/main/java/com/techmania/jalsahay/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.techmania.jalsahay
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Html
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.techmania.jalsahay.databinding.ActivityLoginBinding
+import com.techmania.jalsahay.models.User
 
 class LoginActivity : AppCompatActivity() {
     val binding by lazy {
@@ -19,5 +24,37 @@ class LoginActivity : AppCompatActivity() {
         binding.signin.text = Html.fromHtml(
                     "<font color=#1E88E5> Create a new account</font>"
         )
+        binding.signin.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
+            finish()
+        }
+        binding.loginBtn.setOnClickListener {
+            if (binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")
+            ) {
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Please fill the email and password",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                var user = User(binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                )
+                Firebase.auth.signInWithEmailAndPassword(user.email!!, user.password!!)
+                    .addOnCompleteListener{
+                        if(it.isSuccessful){
+                            startActivity(Intent(this@LoginActivity, HomeActivity :: class.java))
+                            finish()
+                        }else{
+                            Toast.makeText(
+                                this@LoginActivity,
+                                it.exception?.localizedMessage,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+        }
     }
 }
--- app/src/main/java/com/techmania/jalsahay/models/User.kt
+++ app/src/main/java/com/techmania/jalsahay/models/User.kt
@@ -4,6 +4,7 @@ class User {
     var name: String = ""
     var email: String = ""
     var password: String = ""
+    var image: String ?= null
 
     constructor()
     constructor(name: String, email: String, password: String) {
@@ -11,5 +12,18 @@ class User {
         this.email = email
         this.password = password
     }
+
+    constructor(email: String, password: String) {
+        this.email = email
+        this.password = password
+    }
+
+    constructor(name: String, email: String, password: String, image: String?) {
+        this.name = name
+        this.email = email
+        this.password = password
+        this.image = image
+    }
+
 
 }
