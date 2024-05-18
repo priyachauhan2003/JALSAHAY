7029df9a994fc816c60172e375cfaf9e54633ac1
Vansh Punia <vanshpunia007@gmial.com>
2024-05-18 14:07:07 +0530

    test commit

--- app/src/main/java/com/techmania/jalsahay/HomeActivity.kt
+++ app/src/main/java/com/techmania/jalsahay/HomeActivity.kt
@@ -19,7 +19,7 @@ class HomeActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        window.statusBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.BLACK
 
         val navView: BottomNavigationView = binding.navView
 
